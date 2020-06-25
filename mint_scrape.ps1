8f0331a4051fad0af1100d37004681a0bc942b7d
Evan Reid <evan@reids.us>
2020-06-24 22:05:47 -0700

    Handle edge cases

--- mint_scrape.ps1
+++ mint_scrape.ps1
@@ -2,41 +2,70 @@ $URL = 'https://www.mint.com/'
 $Site = Invoke-WebRequest $URL
 
 # Login Information
-$USER = 'evanr777@protonmail.com'
-$PASS = 'A2uqbg2x!'
+$USER = ''
+$PASS = ''
 
-$Driver = Start-SeFirefox -Headless
+$Driver = Start-SeFirefox
 # TODO use chrome
 # $ChromeDriver = New-Object OpenQA.Selenium.Chrome.ChromeDriver
 
+Write-Output `n
+
 Enter-SeUrl -Driver $Driver -Url 'https://www.mint.com/'
 
-# todo wait
-start-sleep -Milliseconds 1000
-
 # click sign in button in upper right hand 
-Find-SeElement -Driver $Driver -XPath '/html/body/div[1]/div/section[1]/header/div/div[3]/a[2]' | Invoke-SeClick
-
-start-sleep -Milliseconds 1000
+Find-SeElement -Driver $Driver -Timeout 2 -XPath '/html/body/div[1]/div/section[1]/header/div/div[3]/a[2]' | Invoke-SeClick
 
 # enter username
-$UserInput = Find-SeElement -Driver $Driver -XPath '//*[@id="ius-userid"]'
+$UserInput = Find-SeElement -Driver $Driver -Timeout 2 -XPath '//*[@id="ius-userid"]'
 Send-SeKeys -Element $UserInput -Keys "$USER"
 
 # enter password
-$PasswordInput = Find-SeElement -Driver $Driver -XPath '//*[@id="ius-password"]'
+$PasswordInput = Find-SeElement -Driver $Driver -Timeout 2 -XPath '//*[@id="ius-password"]'
 Send-SeKeys -Element $PasswordInput -Keys "$PASS"
 
 # click login page sign in button 
-Find-SeElement -Driver $Driver -XPath '//*[@id="ius-sign-in-submit-btn"]' | Invoke-SeClick
+Find-SeElement -Driver $Driver -Timeout 2 -XPath '//*[@id="ius-sign-in-submit-btn"]' | Invoke-SeClick
 
-start-sleep -Milliseconds 5000
+# wait for goals dialog to show, and close
+Find-SeElement -Driver $Driver -Timeout 20 -XPath '/html/body/div[8]/div/div[3]/div/div/button' | Invoke-SeClick
+
+Start-Sleep -Milliseconds 1000
+$Body = $Driver.FindElementsByCssSelector('#body-mint');
+$NumPress = 12
+
+# press down 10 times to bring accounts into view
+while ($NumPress -gt 0) {
+  $Body.SendKeys([OpenQA.Selenium.Keys]::Down)
+  start-sleep -Milliseconds 100
+  $NumPress--
+}
+
+# Open account sections if not currently open
+$CashCaret = $Driver.FindElementsByCssSelector('#moduleAccounts-bank');
+$CreditCaret = $Driver.FindElementsByCssSelector('#moduleAccounts-credit')
+$InvestmentCaret = $Driver.FindElementsByCssSelector('#moduleAccounts-investment')
+
+# TODO create methods
+if (!($CashCaret.GetAttribute("class") -split " " -contains "open")) {
+  $CashCaret.Click()
+}
+
+if (!($CreditCaret.GetAttribute("class") -split " " -contains "open")) {
+  $CreditCaret.Click()
+}
+
+if (!($InvestmentCaret.GetAttribute("class") -split " " -contains "open")) {
+  $InvestmentCaret.Click()
+}
 
 #TODO print account name, category, net worth
-# TODO zoom out
-# TODO headless
 $CashAccounts = $Driver.FindElementsByClassName('accounts-data-li')
 foreach ($account in $CashAccounts) {
+  $accountName = $account.FindElementByClassName('accountName')
   $balance = $account.FindElementByClassName('balance')
-  Write-Output $balance.Text
+  $output = $accountName.Text + ":       " + $balance.Text
+  Write-Output $output
 }
+
+$Driver.Quit()
