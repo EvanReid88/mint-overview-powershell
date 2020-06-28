4d9ef8a5116e29b484bd6edb8b615ec596db6049
Evan Reid <evan@reids.us>
2020-06-27 23:36:11 -0700

    Optimize functions

--- mint_scrape.ps1
+++ mint_scrape.ps1
@@ -1,24 +1,61 @@
 # FUNCTIONS
-Function Write-AccountInfo ($accountList, $accountType) {
-  $AccountBalanceElement = $accountList.FindElementByClassName('balance')
-  $AccountListElement = $accountList.FindElementsByClassName('accounts-data-li')
-  $outputList = New-Object System.Collections.ArrayList
 
-  foreach ($account in $AccountListElement) {
-    $accountName = $account.FindElementByClassName('accountName')
-    $balance = $account.FindElementByClassName('balance')
+# WebDriver Functions
+Function Write-AccountInfo ($AccountList, $AccountType) {
+  $AccountBalanceElement = $AccountList.FindElementByClassName('balance')
+  $AccountListElement = $AccountList.FindElementsByClassName('accounts-data-li')
+  $OutputList = New-Object System.Collections.ArrayList
+
+  foreach ($Account in $AccountListElement) {
+    $AccountName = $Account.FindElementByClassName('accountName')
+    $Balance = $Account.FindElementByClassName('balance')
   
     $temp = New-Object System.Object
-    $temp | Add-Member -MemberType NoteProperty -Name "Account Name" -Value $accountName.Text
-    $temp | Add-Member -MemberType NoteProperty -Name "Balance" -Value $balance.Text
+    $temp | Add-Member -MemberType NoteProperty -Name "Account Name" -Value $AccountName.Text
+    $temp | Add-Member -MemberType NoteProperty -Name "Balance" -Value $Balance.Text
   
-    $outputList.add($temp) | Out-Null
+    $OutputList.add($temp) | Out-Null
   }
 
-  # Print account type title and total
-  Write-Host -NoNewLine "$($accountType): $($AccountBalanceElement.Text)"
-  # Print individual account information
-  $outputList | Format-Table -Property *
+  # print account type title and total
+  Write-Host -NoNewLine "$($AccountType): $($AccountBalanceElement.Text)"
+  # print individual account information
+  $OutputList | Format-Table -Property *
+}
+
+Function Unlock-MintDashboard ($Driver) {
+  # navigate to mint website
+  Enter-SeUrl -Driver $Driver -Url 'https://www.mint.com/'
+
+  # click sign in button in upper right hand 
+  Find-SeElement -Driver $Driver -Timeout 2 -XPath '/html/body/div[1]/div/section[1]/header/div/div[3]/a[2]' | Invoke-SeClick
+  
+  # enter username
+  $UserInput = Find-SeElement -Driver $Driver -Timeout 2 -XPath '//*[@id="ius-userid"]'
+  Send-SeKeys -Element $UserInput -Keys "$USER"
+  
+  # enter password
+  $PasswordInput = Find-SeElement -Driver $Driver -Timeout 2 -XPath '//*[@id="ius-password"]'
+  Send-SeKeys -Element $PasswordInput -Keys "$PASS"
+  
+  # click login page sign in button 
+  Find-SeElement -Driver $Driver -Timeout 2 -XPath '//*[@id="ius-sign-in-submit-btn"]' | Invoke-SeClick
+}
+
+Function Format-MintDashboard ($Driver) {
+  # wait for goals dialog to show, and close
+  Find-SeElement -Driver $Driver -Timeout 20 -XPath '/html/body/div[8]/div/div[3]/div/div/button' | Invoke-SeClick
+  
+  Start-Sleep -Milliseconds 300
+  $Body = $Driver.FindElementsByCssSelector('#body-mint');
+  $NumPress = 18
+  
+  # press down 10 times to bring accounts into view
+  while ($NumPress -gt 0) {
+    $Body.SendKeys([OpenQA.Selenium.Keys]::Down)
+    start-sleep -Milliseconds 100
+    $NumPress--
+  }
 }
 
 Function Write-NetWorth($Driver) {
@@ -26,6 +63,11 @@ Function Write-NetWorth($Driver) {
   Write-Host "Net Worth: $($NetWorthElement.Text)"
 }
 
+Function Open-AccountList($AccountList) {
+  if (!($AccountList.GetAttribute("class") -split " " -contains "open")) {
+    $AccountList.Click()
+  }
+}
 
 # SCRIPT
 $URL = 'https://www.mint.com/'
@@ -34,69 +76,34 @@ $Site = Invoke-WebRequest $URL
 # Login Information
 $USER = ''
 $PASS = ''
+$Driver = Start-SeChrome -Minimized
 
-$Driver = Start-SeChrome
-# TODO use chrome
-# $ChromeDriver = New-Object OpenQA.Selenium.Chrome.ChromeDriver
+Unlock-MintDashboard $Driver
+Format-MintDashboard $Driver
 
-Write-Output `n
+# TODO use enums to map account types to selectors
+# TODO create const for selectors
 
-Enter-SeUrl -Driver $Driver -Url 'https://www.mint.com/'
-
-# click sign in button in upper right hand 
-Find-SeElement -Driver $Driver -Timeout 2 -XPath '/html/body/div[1]/div/section[1]/header/div/div[3]/a[2]' | Invoke-SeClick
-
-# enter username
-$UserInput = Find-SeElement -Driver $Driver -Timeout 2 -XPath '//*[@id="ius-userid"]'
-Send-SeKeys -Element $UserInput -Keys "$USER"
-
-# enter password
-$PasswordInput = Find-SeElement -Driver $Driver -Timeout 2 -XPath '//*[@id="ius-password"]'
-Send-SeKeys -Element $PasswordInput -Keys "$PASS"
-
-# click login page sign in button 
-Find-SeElement -Driver $Driver -Timeout 2 -XPath '//*[@id="ius-sign-in-submit-btn"]' | Invoke-SeClick
-
-# wait for goals dialog to show, and close
-Find-SeElement -Driver $Driver -Timeout 20 -XPath '/html/body/div[8]/div/div[3]/div/div/button' | Invoke-SeClick
-
-Start-Sleep -Milliseconds 300
-$Body = $Driver.FindElementsByCssSelector('#body-mint');
-$NumPress = 18
-
-# press down 10 times to bring accounts into view
-while ($NumPress -gt 0) {
-  $Body.SendKeys([OpenQA.Selenium.Keys]::Down)
-  start-sleep -Milliseconds 100
-  $NumPress--
-}
-
-# Open account sections if not currently open
-$CashList = $Driver.FindElementsByCssSelector('#moduleAccounts-bank');
+# open account sections if not currently open
+$CashList = $Driver.FindElementsByCssSelector('#moduleAccounts-bank')
 $CreditList = $Driver.FindElementsByCssSelector('#moduleAccounts-credit')
 $InvestmentList = $Driver.FindElementsByCssSelector('#moduleAccounts-investment')
 
-# TODO create methods
-if (!($CashList.GetAttribute("class") -split " " -contains "open")) {
-  $CashList.Click()
-}
+# open account sections if not opened
+Open-AccountList $CashList
+Open-AccountList $CreditList
+Open-AccountList $InvestmentList
 
-if (!($CreditList.GetAttribute("class") -split " " -contains "open")) {
-  $CreditList.Click()
-}
+# display Net Worth, Account Categories and Account Information
+Write-Host `n
+Write-NetWorth $Driver
+Write-Host `n
 
-if (!($InvestmentList.GetAttribute("class") -split " " -contains "open")) {
-  $InvestmentList.Click()
-}
-
-$accountList = New-Object System.Collections.ArrayList
-
-Write-NetWorth
-#TODO print account name, category, net worth
 Write-AccountInfo $CashList "Cash"
 Write-AccountInfo $CreditList "Credit"
 Write-AccountInfo $InvestmentList "Investments"
 
-#$Driver.Quit()
+# close Browser
+$Driver.Quit()
 
 
